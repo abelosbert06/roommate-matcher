d25aac6251867a7d4c89eeffcca9d060ace18cdf
abelosbert06 <abelosbert06@gmail.com>
2025-02-11 15:02:27 +0530

    implement storing responses in a JSON

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
 
 
 app.use(express.static("public"));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 app.listen(port, () => {
@@ -17,9 +20,33 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
+
 app.post("/submit", (req, res) => {
+
+
+    const filePath = 'data.json';
+
+    // Read existing data
+    let existingData = [];
+    if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        if (fileContent) {
+            existingData = JSON.parse(fileContent);
+        }
+    }
+
+    // Append new data
+    existingData.push(req.body);
+
+    // Write updated data back to file
+    fs.writeFileSync(filePath, JSON.stringify(existingData, null, 2));
+
+
+
     console.log(req.body);
     res.render("submit.ejs", {
         name: req.body["student-name"],
     })
+
+    
 })
